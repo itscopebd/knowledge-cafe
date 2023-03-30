3a6ecf1806c29b25f3fbd154ec58b1d72fb80076
itscopebd <itscopebd@gmail.com>
2023-03-30 23:04:19 +0600

    Load fake data and show data ui

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SingleBlog from './../singleBlog/SingleBlog';
 
 const Blog = () => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch("fakeData.json")
+            .then(res => res.json())
+            .then(data => setData(data))
+    }, []);
+
     return (
         <div>
-            <SingleBlog></SingleBlog>
+            {
+                data.map(singleBlog=><SingleBlog singleBlog={singleBlog} key={singleBlog.id}></SingleBlog>)
+            }
         </div>
     );
 };
--- src/components/singleBlog/SingleBlog.jsx
+++ src/components/singleBlog/SingleBlog.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const SingleBlog = () => {
+const SingleBlog = (props) => {
+    const {authorImage,authorName,blogCoverImage,blogTitle,publishDate,readTime,tag}=props.singleBlog;
+    // console.log(a)
     return (
         <div>
-            
+            <img src={blogCoverImage} alt="" />
+            <div>
+                <img src={authorImage} alt="" />
+                <div>
+                    <h4>{authorName}</h4>
+                    <p>{publishDate}</p>
+                </div>
+                <p>{readTime} min read </p>
+            </div>
+            <h2>{blogTitle}</h2>
+            <button>Mark as read</button>
         </div>
     );
 };
