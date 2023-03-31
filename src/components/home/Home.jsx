134ad89de85cd8552cf5da4d0f03d700b4f96c57
itscopebd <itscopebd@gmail.com>
2023-03-31 16:54:15 +0600

    Book mark counter and title add functionality added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,30 +1,41 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Blog from '../blog/Blog';
 import Header from '../header/Header';
 import RightSideBar from '../rightSide/RightSideBar';
+import QuestionAnswer from '../questionAnswer/QuestionAnswer';
 
 const Home = () => {
 
-    const[readTime,setReadTime]=useState(0)
-    const readTimeHandle=(time)=>{
-        
-        const totalReadTime= parseInt(time)+ readTime;
+    const [readTime, setReadTime] = useState(0);
+
+    const [totalBookMark, setTotalBookMark] = useState(0);
+
+    const readTimeHandle = (time) => {
+        const totalReadTime = parseInt(time) + readTime;
         setReadTime(totalReadTime)
     }
-const [totalBookMark,setTotalBookMark]=useState(0)
-const bookMark=(id,blogTitle)=>{
-    setTotalBookMark(totalBookMark+1)
-}
-console.log(totalBookMark)
+
+    const [title, setTile] = useState([]);
+    const bookMark = (blogTitle) => {
+
+      const newtitle=[...title,blogTitle]
+        setTile(newtitle);
+        setTotalBookMark(totalBookMark + 1);
+
+    }
 
     return (
         <>
-           <header>
-            <Header></Header>
-            </header> 
-            <main className='flex'>
-                <Blog readTimeHandle={readTimeHandle} bookMark={bookMark}></Blog>
-                <RightSideBar readTime={readTime} totalBookMark={totalBookMark}></RightSideBar>
+            <header>
+                <Header></Header>
+            </header>
+            <main>
+                <div className='flex'>
+                    <Blog readTimeHandle={readTimeHandle} bookMark={bookMark}></Blog>
+                    <RightSideBar readTime={readTime} title={title} totalBookMark={totalBookMark}></RightSideBar>
+                </div>
+                <QuestionAnswer></QuestionAnswer>
+
             </main>
         </>
     );
--- src/components/rightSide/RightSideBar.jsx
+++ src/components/rightSide/RightSideBar.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 const RightSideBar = (props) => {
+    const{title,readTime,totalBookMark,}=props;
     return (
         <div className='w-4/12 ml-5'>
-           <h1 className='text-primary p-3 border-2 border-slate-300 rounded-lg'>Spent time on read : {props.readTime} min</h1>
+           <h1 className='text-primary p-3 border-2 border-slate-300 rounded-lg'>Spent time on read : {readTime} min</h1>
             <div className='p-5 bg-gray-200 my-5 rounded-md'>
-                <h4 className='font-bold text-2xl my-5'>Bookmarked Blogs : {props.totalBookMark}</h4>
+                <h4 className='font-bold text-2xl my-5'>Bookmarked Blogs : {totalBookMark}</h4>
+                 {
+                    title.map((singleTitle,index)=>{
+                  return(
+                    <div className='' key={index+1}>
+                    <h3 className='bg-white p-3 my-3 rounded-lg'>{singleTitle}</h3>
+                    </div>
+                  )
+                    })
+                 }
+                
             </div>
         </div>
     );
